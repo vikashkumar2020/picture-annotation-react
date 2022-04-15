9d7820c26692c36948a8a2b9a2d99ef7128b8709
TensorEleven <harivonyratefiarison@gmail.com>
2022-04-15 13:15:50 +0300

    ADD roughjs, draw ok

--- src/components/Annotate.js
+++ src/components/Annotate.js
@@ -1,10 +1,34 @@
 import React, {useLayoutEffect,useState}from "react";
+import { RoughCanvas } from "roughjs/bin/canvas";
+import rough from 'roughjs/bundled/rough.esm'
 //const document = require('../assets/sample_doc.png')
 
+const generator = rough.generator()
+
+const canvaWidth = 500
+const canvaHeight = 809
+
 export default function Annotate (){
+    useLayoutEffect(()=>{
+        const canvas = document.getElementById("canvas")
+        const ctx = canvas.getContext("2d")
+
+        const roughtCanvas = rough.canvas(canvas)
+        const rect = generator.rectangle(10,10,100,100)
+        roughtCanvas.draw(rect)
+    })
+
     return (
         <>
-        
+            <canvas 
+                id="canvas" 
+                style={{backgroundColor:'#f5f5f5',border:'solid 0.5px grey'}} 
+                width={canvaWidth} 
+                height={canvaHeight}>
+
+
+                Canvas
+            </canvas>
         </>
     )
 }
