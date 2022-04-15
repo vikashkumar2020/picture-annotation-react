868ec8cf9ddbb0876ab635e9fae1819bb55a40be
TensorEleven <harivonyratefiarison@gmail.com>
2022-04-15 15:18:05 +0300

    UPDATE draw rectangle ok

--- src/components/Annotate.js
+++ src/components/Annotate.js
@@ -1,4 +1,4 @@
-import React, {useLayoutEffect,useState}from "react";
+import React, { useLayoutEffect,useState}from "react";
 import { RoughCanvas } from "roughjs/bin/canvas";
 import rough from 'roughjs/bundled/rough.esm'
 //const document = require('../assets/sample_doc.png')
@@ -8,17 +8,62 @@ const generator = rough.generator()
 const canvaWidth = 500
 const canvaHeight = 809
 
+const createLine = (x1,y1,x2,y2) =>{
+    const line = generator.line(x1,y1,x2,y2)
+    return {x1,y1,x2,y2,line}
+}
+
+const createRectangle = (x1,y1,x2,y2) =>{
+    const roughtElement = generator.rectangle(x1,y1,x2-x1,y2-y1)
+    return {x1,y1,x2,y2,roughtElement}
+}
+
 export default function Annotate (){
+    const [elements, setElements] = useState([])
+    const [drawing,setDrawing] = useState(false)
+    
+
     useLayoutEffect(()=>{
         const canvas = document.getElementById("canvas")
-        const ctx = canvas.getContext("2d")
+        const context = canvas.getContext("2d")
+        context.clearRect(0,0,canvas.width,canvas.height)
 
         const roughtCanvas = rough.canvas(canvas)
-        const rect = generator.rectangle(10,10,100,100)
-        const line = generator.line(10,10,110,110)
-        roughtCanvas.draw(rect)
-        roughtCanvas.draw(line)
-    })
+        elements.forEach(element=>{
+            roughtCanvas.draw(element.roughtElement)
+        })
+
+        // const rect = generator.rectangle(10,10,100,100)
+        // const line = generator.line(10,10,110,110)
+        // roughtCanvas.draw(rect)
+        // roughtCanvas.draw(line)
+    },[elements])
+
+    const handleMouseDown = (event)=>{
+        setDrawing(true)
+        const {clientX,clientY} = event
+        const element = createRectangle(clientX,clientY,clientX,clientY)
+        setElements((previousState)=>[...previousState,element])
+
+
+    }
+    const handleMouseMove = (event)=>{
+        if(!drawing) return
+        
+        const {clientX,clientY} = event
+        const index = elements.length -1
+        const {x1,y1} = elements[index]
+        const updatedElement = createRectangle(x1,y1,clientX,clientY)
+        
+        const elementCopy = [...elements]
+        elementCopy[index] = updatedElement
+
+        setElements(elementCopy)
+        //console.log(clientX,clientY)
+    }
+    const handleMouseUp = () =>{
+        setDrawing(false)
+    }
 
     return (
         <>
@@ -26,8 +71,12 @@ export default function Annotate (){
                 id="canvas" 
                 style={{backgroundColor:'#f5f5f5',border:'solid 0.5px grey'}} 
                 width={canvaWidth} 
-                height={canvaHeight}>
-
+                height={canvaHeight}
+                onMouseDown={handleMouseDown}
+                onMouseMove={handleMouseMove}
+                onMouseUp={handleMouseUp}
+                >
+                
 
                 Canvas
             </canvas>
