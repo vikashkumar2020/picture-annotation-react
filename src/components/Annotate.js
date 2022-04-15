ed30a6c1d1e124b46d621f795fb9e239248e8b8c
TensorEleven <harivonyratefiarison@gmail.com>
2022-04-15 12:16:17 +0300

    Initial commit

---
+++ src/components/Annotate.js
@@ -0,0 +1,10 @@
+import React, {useLayoutEffect,useState}from "react";
+//const document = require('../assets/sample_doc.png')
+
+export default function Annotate (){
+    return (
+        <>
+        
+        </>
+    )
+}
