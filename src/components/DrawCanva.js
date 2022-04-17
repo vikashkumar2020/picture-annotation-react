71c81fb39550c07aff262f34a0233d3fc8e5db55
TensorEleven <harivonyratefiarison@gmail.com>
2022-04-17 22:14:36 +0300

    UPDATE get item position onMount and on WindowResize

--- src/components/DrawCanva.js
+++ src/components/DrawCanva.js
@@ -1,4 +1,5 @@
-import React, { useLayoutEffect,useState}from "react";
+import React, { useLayoutEffect,useRef,useState}from "react";
+import { useEffect } from "react";
 import rough from 'roughjs/bundled/rough.esm'
 //const document = require('../assets/sample_doc.png')
 
@@ -6,6 +7,7 @@ const generator = rough.generator()
 
 const canvaWidth = 500
 const canvaHeight = 809
+
 
 const createLine = (x1,y1,x2,y2) =>{
     const line = generator.line(x1,y1,x2,y2)
@@ -15,12 +17,33 @@ const createLine = (x1,y1,x2,y2) =>{
 const createRectangle = (x1,y1,x2,y2) =>{
     const roughtElement = generator.rectangle(x1,y1,x2-x1,y2-y1)
     return {x1,y1,x2,y2,roughtElement}
+
 }
 
-export default function Annotate (){
+const getItemPosition = (itemRef) =>{
+    const posX = itemRef.current.offsetLeft
+    const posY = itemRef.current.offsetRight
+    return {posX,posY}
+}
+
+export default function DrawCanva (){
     const [elements, setElements] = useState([])
     const [drawing,setDrawing] = useState(false)
-    
+    const canvaRef = useRef()
+
+    // get initial position
+    useEffect(()=>{
+        const {posX,posY} = getItemPosition(canvaRef)
+        console.log(posX,posY)
+    },[])
+
+    // get updated position
+    useEffect(()=>{
+        window.addEventListener("resize",()=>{
+            const {posX,posY} = getItemPosition(canvaRef)
+            console.log(posX,posY)
+        })
+    },[])
 
     useLayoutEffect(()=>{
         const canvas = document.getElementById("canvas")
@@ -74,6 +97,7 @@ export default function Annotate (){
                 onMouseDown={handleMouseDown}
                 onMouseMove={handleMouseMove}
                 onMouseUp={handleMouseUp}
+                ref={canvaRef}
                 >
                 Canvas
             </canvas>
